25106c8e02e3138e5c6182845a2e41bc5411ab51
draxano <muradalaskarov@outlook.com>
2022-03-13 08:47:10 -0700

    Some comments for my approach

--- NewBank/newbank/server/NewBank.java
+++ NewBank/newbank/server/NewBank.java
@@ -16,7 +16,7 @@ public class NewBank {
     // then put the customer into the HashMap (the bank)
     private void addTestData() {
         Customer bhagy = new Customer();
-        bhagy.addAccount(new Account("Main", 1000.0));
+        bhagy.addAccount(new Account("Main", 1000.0));// we will have to work with this to make an account
         customers.put("Bhagy", bhagy);
 
         Customer christina = new Customer();
@@ -35,19 +35,24 @@ public class NewBank {
     // creates customer ID when login details are checked. The username is the key
     public synchronized CustomerID checkLogInDetails(String userName, String password) {
         if (customers.containsKey(userName)) {
-            return new CustomerID(userName);
+            return new CustomerID(userName); // so it seems like the customer id will be equal to the user name.
         }
         return null;
     }
 
     // commands from the NewBank customer are processed in this method
-    public synchronized String processRequest(CustomerID customer, String request) {
+    public synchronized String processRequest(CustomerID customer, String request) {  // in here customerId value is string which contains the name, e.g., if name is John the key is John
         // current checks if the HashTable has the username (key) inside
         if (customers.containsKey(customer.getKey())) {
             // if the request says SHOWMYACCOUNTS (with the correct key), then accounts will be shown
             if (request.equals("SHOWMYACCOUNTS") || request.equals("1")) {
                 return showMyAccounts(customer);
             } else if (request.toLowerCase().contains("newaccount") || request.toLowerCase().equals("2")) {
+               // if(customer.getKey().equals("Bhagy")){// this basically checks if the user was Bhagy
+                    // then create an account for baggy and etc.
+                //} this aproach doesnt work since the test data are not global and I tried to make them static and did not work out well.
+                // so we shall actually create a method so that real users can go through this login and they are saved in the hashmap
+
                 return "Create new account - TBD";
             } else if (request.toLowerCase().contains("move") || request.toLowerCase().equals("3")) {
                 return "Move money - TBD";
---
+++ NewBank/newbank/server/Testing.java
@@ -0,0 +1,19 @@
+package newbank.server;
+
+import java.util.HashMap;
+
+public class Testing {
+    public static void main(String[] args){
+
+        HashMap<String, Integer> tests = new HashMap<>();
+
+        tests.put("Hello", 2);
+
+        System.out.println(tests.containsKey("Hello")); // this is sufficient
+
+
+
+
+
+    }
+}
