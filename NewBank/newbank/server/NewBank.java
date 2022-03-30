fb6194b047808ee746517d234379cd1bbc273640
Ibi <ibitayola@gmail.com>
2022-03-30 23:29:21 +0100

    -

--- NewBank/newbank/server/NewBank.java
+++ NewBank/newbank/server/NewBank.java
@@ -116,21 +116,21 @@ public class NewBank {
         return (customers.get(customer.getKey())).accountsToString();
     }
 
-    // Create a new account given an existing cutomer Id
-    private String newAccount(CustomerID customerid, String name) {
-        if (!customers.containsKey(customerid.getKey())) {
-            return "FAIL";
-        } else if (name.length() < 1) {
-            return "FAIL";
-        }
-        Customer customer = customers.get(customerid.getKey());
-        if (customer.hasAccount(name)) {
-            return "FAIL - Account already exists";
-        }
-        Account account = new Account(name, 0.0);
-        customer.addAccount(account);
-        return account.toString();
-    }
+//    // Create a new account given an existing cutomer Id
+//    private String newAccount(CustomerID customerid, String name) {
+//        if (!customers.containsKey(customerid.getKey())) {
+//            return "FAIL";
+//        } else if (name.length() < 1) {
+//            return "FAIL";
+//        }
+//        Customer customer = customers.get(customerid.getKey());
+//        if (customer.hasAccount(name)) {
+//            return "FAIL - Account already exists";
+//        }
+//        Account account = new Account(name, 0.0);
+//        customer.addAccount(account);
+//        return account.toString();
+//    }
 
     private String move(CustomerID customer, String amount, String from, String to) {
         return "Move money - TBD";
