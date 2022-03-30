f9964a664a085b6dfc00320f47091d16c7d0a31f
Ibi <ibitayola@gmail.com>
2022-03-30 18:23:30 +0100

    new method that adds an account to the database new method that retrieves all accounts tied to a given user new method that returns a user's id from the database (used in addAccount method)

--- NewBank/newbank/database/dbOperations.java
+++ NewBank/newbank/database/dbOperations.java
@@ -1,5 +1,6 @@
 package newbank.database;
 
+import newbank.server.Account;
 import newbank.server.Customer;
 // jbcrypt library helps take passwords and store them as hashes in the database
 // using a cryptographic hash algorithm called 'bcrypt'.
@@ -8,18 +9,19 @@ import org.sqlite.SQLiteException;
 
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class dbOperations {
 
-    // for inserting data into the database table.
+    // for creating a user account
     public static boolean insert(String username, String password) {
         // hash the password first for security, then store the hash in the database
         String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
         String sql = "INSERT into users(username, password) VALUES(?,?)";
 
         try (Connection con = dbConnection.connect(); PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setString(1, username);
+            ps.setString(1, username.toLowerCase());
             ps.setString(2, hashed);
             ps.execute();
             System.out.println("\"" + username + "\"" + " added to database.");
@@ -31,6 +33,68 @@ public class dbOperations {
             e.printStackTrace();
         }
         return false;
+    }
+
+    // for opening a new bank account and linking that account to a user
+    public static boolean addAccount(String username, String accountName, double startingBalance) {
+        int accountId = -1;
+        int userId;
+
+        // get user id
+        userId = getUserId(username);
+
+        String sql = "INSERT into accounts(type, balance) VALUES(?,?)";
+        try (Connection con = dbConnection.connect(); PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            con.setAutoCommit(false);
+            ps.setString(1, accountName);
+            ps.setDouble(2, startingBalance);
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                accountId = rs.getInt(1);
+            }
+
+            // link user and account and put into mapping table
+            if (accountId > 0 && userId > 0) {
+                String mappingSql = "INSERT into accountmappings(userid, accountid) VALUES(?,?)";
+                PreparedStatement link = con.prepareStatement(mappingSql);
+                link.setInt(1, userId);
+                link.setInt(2, accountId);
+                link.executeUpdate();
+                con.commit(); // save changes made to db
+                System.out.println("For: " + username + ", a new " + accountName + " account has been opened with starting balance of " + startingBalance);
+
+            } else {
+                con.rollback(); // reverse changes made to the db
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // for getting account information for a given customer.
+    // returns an arraylist containing all the user's accounts
+    public static ArrayList<Account> getAccounts(String username) {
+        ArrayList<Account> accounts = new ArrayList<>();
+        String sqlQuery = "SELECT type, balance FROM users a JOIN accountmappings b ON a.id = b.userid " +
+                "JOIN accounts c on c.id = b.accountid WHERE username =?";
+
+        try (Connection con = dbConnection.connect(); PreparedStatement ps = con.prepareStatement(sqlQuery)) {
+            ps.setString(1, username.toLowerCase());
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                accounts.add(new Account(rs.getString(1), rs.getDouble(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (accounts.isEmpty()) {
+            System.err.println("No accounts found for this user.");
+        }
+        return accounts;
     }
 
     // for deleting data from the database table
@@ -55,7 +119,7 @@ public class dbOperations {
         String sqlQuery = "SELECT password FROM users where username =?";
 
         try (Connection con = dbConnection.connect(); PreparedStatement ps = con.prepareStatement(sqlQuery)) {
-            ps.setString(1, username);
+            ps.setString(1, username.toLowerCase());
 
             ResultSet rs = ps.executeQuery();
             // if the user exists then the result set will contain a hashed password
@@ -97,11 +161,35 @@ public class dbOperations {
         return map;
     }
 
+    // get a user's id from the database
+    public static int getUserId(String username) {
+        int userId = -1;
+        String sqlQuery = "SELECT id FROM users where username =?";
+        try (Connection con = dbConnection.connect(); PreparedStatement ps = con.prepareStatement(sqlQuery)) {
+            ps.setString(1, username.toLowerCase());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                userId = rs.getInt(1);
+            } else {
+                System.err.println("User does not exist.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userId;
+    }
+
+
     // used to insert the test customers into database
     public static void main(String[] args) {
-        insert("Bhagy", "password1");
-        insert("John", "password1");
-        insert("Christina", "password1");
+//        insert("Bhagy", "password1");
+//        insert("John", "password1");
+//        insert("Christina", "password1");
+
+        //addAccount("Bhagy", "savings", 30000);
+        // addAccount("christina", "savings", 23300);
+
+        //getAccounts("bhagy");
     }
 
 }
