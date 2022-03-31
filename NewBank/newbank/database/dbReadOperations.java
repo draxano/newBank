08683e066579676f0f80dc1807dfe2b5219da879
Ibi <ibitayola@gmail.com>
2022-04-01 00:43:40 +0100

    two methods added here to obtain account information. these can be used to make more specific changes to any users account

--- NewBank/newbank/database/dbReadOperations.java
+++ NewBank/newbank/database/dbReadOperations.java
@@ -33,6 +33,51 @@ public class dbReadOperations {
         return accounts;
     }
 
+    // get an account object of a specified user's account
+    public static Account getAccount(String userName, String accountName) {
+        Account account = null;
+        String sqlQuery = "SELECT type, balance " +
+                "FROM users a JOIN accountmappings b ON a.id = b.userid JOIN accounts c on c.id = b.accountid " +
+                "WHERE username =? AND type =?";
+        try (Connection con = dbConnection.connect(); PreparedStatement ps = con.prepareStatement(sqlQuery)) {
+            ps.setString(1, userName);
+            ps.setString(2, accountName);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                account = new Account(rs.getString(1), rs.getDouble(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return account;
+    }
+
+    // get the id of a specified user's account
+    public static int getAccountId(String userName, String accountName) {
+        int accountId = -1;
+        String sqlQuery = "SELECT accountid " +
+                "FROM accounts a JOIN accountmappings b ON a.id = b.accountid JOIN users c on c.id = b.userid " +
+                "WHERE username =? AND type =?";
+        try (Connection con = dbConnection.connect(); PreparedStatement ps = con.prepareStatement(sqlQuery)) {
+            ps.setString(1, userName);
+            ps.setString(2, accountName);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                accountId = rs.getInt(1);
+            } else {
+                System.err.println("Account id not found.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+//            if (accountId == -1) {
+//                System.err.println("No such account exists.");
+//            }
+            return accountId;
+    }
+
     // query the database - returns true if username and password match.
     public static boolean checkLogin(String username, String candidate) {
         // query the database and extract hashed password from database
