d7d4c62b3fb4519de4cffa43db1ce25838f332b0
Ibi <ibitayola@gmail.com>
2022-04-01 00:42:50 +0100

    added a method to delete a user account (hasnt been integrated as a feature yet)

--- NewBank/newbank/database/dbDeleteOperations.java
+++ NewBank/newbank/database/dbDeleteOperations.java
@@ -5,7 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class dbDeleteOperations {
-    // for deleting data from the database table
+
+    // for deleting a user from the database table
     public static boolean delete(String username) {
         String sql = "DELETE FROM users WHERE username = ?";
 
@@ -18,5 +19,22 @@ public class dbDeleteOperations {
             e.printStackTrace();
         }
         return false;
+    }
+
+    // method to delete a user account
+    public static boolean deleteAccount(int accountId) {
+        if (accountId < 0) return false;
+
+        boolean success = false;
+        String sqlDelete = "DELETE FROM accounts WHERE id =?";
+        try (Connection con = dbConnection.connect(); PreparedStatement ps = con.prepareStatement(sqlDelete)) {
+            ps.setInt(2, accountId);
+            ps.executeUpdate();
+            success = true;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return success;
     }
 }
